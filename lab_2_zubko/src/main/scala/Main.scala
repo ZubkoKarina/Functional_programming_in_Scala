cfd1f72b88154fbf30d15062eb2690674329603d
Karina Zubko <karinazubko2017@gmail.com>
2023-03-18 11:40:34 +0100

    lab 2

--- lab_2_zubko/src/main/scala/Main.scala
+++ lab_2_zubko/src/main/scala/Main.scala
@@ -1,3 +1,14 @@
+//Zubko
+//task1
 object Main extends App {
-  println("Hello, World!")
+  def equation(x: Double): Double = {
+    x match {
+      case x if x <= -4 => 4 * x * x + 4
+      case x if x > -4 && x < 4 => x * x * x - 1
+      case x if x >= 4 => x * x + 1
+    }
+  }
+  val xValue = 2.0 
+  val yValue = equation(xValue)
+  println(s"y = $yValue")
 }
