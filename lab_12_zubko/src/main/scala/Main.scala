3504bd054d11f2d7f1823aedef729a9e209cc167
Karina Zubko <karinazubko2017@gmail.com>
2023-03-18 12:58:37 +0100

    lab 12

--- lab_12_zubko/src/main/scala/Main.scala
+++ lab_12_zubko/src/main/scala/Main.scala
@@ -1,3 +1,19 @@
-object Main extends App {
-  println("Hello, World!")
+object CustomListFunctions extends App {
+//zubko
+  def lengthList[A](list: List[A]): Int = list.length
+
+  def nthList[A](n: Int, list: List[A]): Option[A] = list.lift(n)
+
+  def removeNegative(list: List[Int]): List[Int] = list.filter(_ >= 0)
+
+  def fromList[A](list: List[A]): List[A] = list
+
+  def toList[A](list: List[A]): List[A] = list
+
+  val sampleList = List(1, -2, 3, -4, 5)
+  println(s"Length of the list: ${lengthList(sampleList)}")
+  println(s"3rd element of the list: ${nthList(2, sampleList)}")
+  println(s"List without negative elements: ${removeNegative(sampleList)}")
+  println(s"From list: ${fromList(sampleList)}")
+  println(s"To list: ${toList(sampleList)}")
 }
